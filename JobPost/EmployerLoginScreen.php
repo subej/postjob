54de40bb622e6c5ff9b821cc12bcc91ad7bbde53
subej <mfm23@nau.edu>
2014-06-13 12:19:27 -0700

    JobPost files. There are a few garbage files here but feel free to look through and get a feel for PHP

---
+++ JobPost/EmployerLoginScreen.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<head>
+  <title>JobPost</title>
+  <link rel="stylesheet" href="tester.css">
+</head>
+
+<h1 align="center">JobPost</h1>
+
+<form action="EmployerInterface.php" method="post">
+Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ JobPost/HomePage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+  <title>JobPost</title>
+  <link rel="stylesheet" href="tester.css">
+</head>
+
+<h1 align="center">JobPost</h1>
+
+<p>Welcome to JobPost! <br>
+Please login as either an employer or student:</p>
+
+<div class="loginbuttons">
+<form action="EmployerLoginScreen.php" method="post">
+<input class="input" type="submit" value ="Login as Employer">
+</form>
+
+<form action="StudentLoginScreen.php" method="post">
+<input class="input" type="submit" value="Login as Student" name="StudentLogin">
+</form>
+</div>
+</body>
+</html>
