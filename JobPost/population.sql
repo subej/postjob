491fb8287f6fbab1245eddd01a4a8c991f53faf4
Anastasiya <anastasiya.ger@gmail.com>
2014-06-13 16:34:13 -0700

    c

---
+++ JobPost/population.sql
@@ -0,0 +1,5 @@
+insert into UNIVERSITY  values (00001, University of Toronto, 27, King's College Circle, Toronto, Ontario, M5S1A1);
+insert into UNIVERSITY  values (00002, The University of British Columbia, 2329 , West Mall, Vancouver, British Columbia, V6T1Z4);
+insert into UNIVERSITY  values (00003, University of Alberta, 116, St. and 85 Ave., Edmonton, Alberta, T6G2R3);
+insert into UNIVERSITY  values (00004, Simon Fraser University, 8888, University Drive, Burnaby, British Columbia, V5A1S6);
+insert into UNIVERSITY  values (00005, McGill University, 845 , Sherbrooke Street West, Montreal, Quebec, H3A2T5);
