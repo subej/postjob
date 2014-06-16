0462bab93c1d8c155b1678e3b11489e064d5701b
subej <mfm23@nau.edu>
2014-06-16 15:07:45 -0700

    Employer login post variables set with conditionals

--- JobPost/EmployerLoginScreen.php
+++ JobPost/EmployerLoginScreen.php
@@ -9,19 +9,15 @@
 <?php
 
 // These variables are extracted from the text boxes each time this page is called 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$test = $_POST["StudentLogin"];
+if(isset($_POST["username"])){$username = $_POST["username"];}
+if(isset($_POST["password"])){$password = $_POST["password"];}
+$test(isset($_POST["StudentLogin"])) = $_POST["StudentLogin"];
 
 // Connect to database: You have to enter your own password and databasename
 $con=mysqli_connect("localhost","root","Compouter25624!","cpsc304");
 
 //Query for username and password entered
-$result2 = mysqli_query($con, "SELECT Username, Password FROM COMPANY WHERE Username='$username' AND Password='$password';");
-
-// Primative Boolean to see if there is a match in the DB for username and password
-$unamepasswordmatch = 1;
-
+if(isset($_POST["username"])){$result2 = mysqli_query($con, "SELECT Username, Password FROM COMPANY WHERE Username='$username' AND Password='$password';");
 // This button will pass the username to the interface page in a post request
 while($row = mysqli_fetch_array($result2)) {
  if($row2['Username'] == $username2){
@@ -32,6 +28,12 @@ while($row = mysqli_fetch_array($result2)) {
    echo '</form>';
  }
 }
+}
+
+// Primative Boolean to see if there is a match in the DB for username and password
+$unamepasswordmatch = 1;
+
+
 
 // If username and password not found, load login buttons and text fields
 if($unamepasswordmatch > 0){
