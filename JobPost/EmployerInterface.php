64eba644a88de052cb99b8d169d2978ffa9fde78
Anastasiya <anastasiya.ger@gmail.com>
2014-06-16 17:57:19 -0700

    OffersAccepted Button
    display the students how has applied

--- JobPost/EmployerInterface.php
+++ JobPost/EmployerInterface.php
@@ -4,7 +4,7 @@
   <title>JobPost</title>
   <link rel="stylesheet" href="tester.css">
     <script type="text/javascript">
-
+t
       function activateTab(pageId) {
           var tabCtrl = document.getElementById('tabCtrl');
           var pageToActivate = document.getElementById(pageId);
@@ -15,10 +15,21 @@
               }
           }
       }
-
+      function showOffers(){
+	  document.getElementById(OffersAccepted).style.display = "block";
+	  }
     </script>
   </head>
   <body>
+  <?php 
+  // THIS IS A TEST
+  if(isset($_POST['username'])){
+    $TEST = $_POST['username'];
+	echo $TEST;
+	} else{
+	  echo "I did not get a post request";
+	}
+  ?>
     <ul>
       <li>
         <a href="javascript:activateTab('page1')">Your Profile</a>
@@ -33,9 +44,61 @@
         <a href="javascript:activateTab('page4')">Offers Pedning</a>
       </li>
       <li>
-        <a href="javascript:activateTab('page5')">Offers Accepted</a>
+       <input type="button" name="Offers Accepted" value="Offers Accepted" onClick="showOffers()"
       </li>
     </ul>
+	<div id="OffersAccepted"
+    		
+    		class="answer_list">
+			<p> I am a test. Can you see me? </p>
+    			<?php
+				//THIS TESTS FOR POST INFO
+				$TEST = $_POST['username'];
+				echo $TEST;
+				if(isset($_POST['username'])){$username = $_POST['username'];}
+				$con = mysqli_connect("localhost","root", "123456","newdb");
+				$coidresult = mysqli_query($con,"SELECT co_id
+				FROM COMPANY WHERE COMPANY.Username = '". $username . "'");
+				if(!$coidresult){ 
+					echo "I didn't query.";
+					die('Error: ' . mysqli_error($con));
+				}
+				$co_id = null;
+				while($row = mysqli_fetch_array($coidresult)){
+					echo $row['co_id'];
+					$co_id = $row['co_id'];
+				}
+				$result = mysqli_query($con,"SELECT * FROM APPLIES A, STUDENT_STUDIES S WHERE S.s_id = A.s_id AND A.co_id =" . $co_id );
+				//change this to a view that also show content of contract later
+				if(!$result){ 
+					echo "I didn't query applications.";
+					die('Error: ' . mysqli_error($con));
+				}
+				echo "<table border='1'>
+				<tr>	
+				<th>Student ID</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Faculty</th>
+				<th>Year</th>	
+				<th>Major</th>
+				
+				</tr>";
+
+				while($row = mysqli_fetch_array($result)) {
+			  		echo "<tr>";	
+			  		echo "<td>" . $row['s_id'] . "</td>";
+  					echo "<td>" . $row['FirstName'] . "</td>";
+  					echo "<td>" . $row['LastName'] . "</td>";
+					echo "<td>" . $row['Faculty'] . "</td>";
+					echo "<td>" . $row['Year'] . "</td>";
+					echo "<td>" . $row['Major'] . "</td>";
+					echo "</tr>";
+				}
+
+				echo "</table>";
+			?>
+		</div>
     <div id="tabCtrl">
       <div id="page1" style="display: block;">Your Profile</div>
       <div id="page2" style="display: none;">Your Postings</div>
