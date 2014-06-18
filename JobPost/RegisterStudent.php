06f446b2b75b4e42c6fe2b9fc37f3ff203980a03
zietlan <azanna.00@gmail.com>
2014-06-17 20:30:30 -0700

    works on first try now
    added isset checks

--- JobPost/RegisterStudent.php
+++ JobPost/RegisterStudent.php
@@ -13,51 +13,83 @@
   $registered = False;
   $totaltrue = 0;
   // Variables from user submission
-  $universityid = $_POST["universityid"];
-  if($universityid > 0){
-    $totaltrue++;
+  if(isset($_POST["universityid"])) {
+  	$universityid = $_POST["universityid"];
+  	  if($universityid > 0){
+    		$totaltrue++;
+  	}
   }
+  
+  if(isset($_POST["studentid"])) {
   $studentid = $_POST["studentid"];
   if($studentid > 0){
     $totaltrue++;
   }
-  $firstname = $_POST["firstname"];
-  if(strlen($firstname) > 0){
-    $totaltrue++;
   }
+  
+  if(isset($_POST["firstname"])) {
+  $firstname = $_POST["firstname"];
+    if(strlen($firstname) > 0){
+    		$totaltrue++;
+  }
+  }
+  
+  if(isset($_POST["lastname"])) {
   $lastname = $_POST["lastname"];
   if(strlen($lastname) > 0){
     $totaltrue++;
   }
+  } 
+  
+  if(isset($_POST["faculty"])){
   $faculty = $_POST["faculty"];
   if(strlen($faculty) > 0){
     $totaltrue++;
   }
+  }
+  
+  if(isset($_POST["year"])) {
   $year = $_POST["year"];
   if($year > 0){
     $totaltrue++;
   }
+  }
+  
+  if(isset($_POST["major"])) {
   $major = $_POST["major"];
   if(strlen($major) > 0){
     $totaltrue++;
   }
+  }
+  
+  if(isset($_POST["username"])) {
   $username = $_POST["username"];
   if(strlen($username) > 0){
     $totaltrue++;
   }
+  }
+  
+  if(isset($_POST["password1"])) {
   $password1 = $_POST["password1"];
   if(strlen($password1) > 0){
     $totaltrue++;
   }
+  }
+  
+  if(isset($_POST["password2"])) {
   $password2 = $_POST["password2"];
   if(strlen($password2) > 0){
     $totaltrue++;
   }
+  }
+  
   // If all 10 fields are filled in, the form was completed
   if($totaltrue == 10){ $registered = True; }
 
   // Variable for which site delievered post request
+  if(isset($_POST["StudentLogin"])) {
   $fromwhere = $_POST["StudentLogin"];
+  }
 
   echo "Welcome to JobPost!";
 
@@ -66,7 +98,7 @@
 
    // Insert user data into database
    
-   $con=mysqli_connect("localhost","root","Compouter25624!","cpsc304");
+   $con=mysqli_connect("127.0.0.1","admin","pass", "JobPost", 3306);
    $insertion = mysqli_query($con,"INSERT INTO STUDENT_STUDIES (s_id, u_id, FirstName, LastName, Faculty, Year, Major, Username, Password)
 VALUES ('$studentid', '$universityid', '$firstname', '$lastname', '$faculty', '$year', '$major', '$username', '$password1')");
 
@@ -88,7 +120,7 @@ VALUES ('$studentid', '$universityid', '$firstname', '$lastname', '$faculty', '$
 
   // This will generate the list of Universities for a user to select from
 
-  $con=mysqli_connect("localhost","root","Compouter25624!","cpsc304");
+  $con=mysqli_connect("127.0.0.1","admin","pass", "JobPost", 3306);
   $universityquery = mysqli_query($con,"SELECT u_id, Name FROM UNIVERSITY;");
   // This will appear if the user is not registered, or not information was entered incorrectly
   echo '<form action="RegisterStudent.php" method="post">';
